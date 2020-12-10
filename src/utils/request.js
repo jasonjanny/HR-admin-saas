e1230dcf2017cd229728820bbc2fbbd13d50fd16
janny <2431349655@qq.com>
2020-12-10 16:23:33 +0800

    获取用户资料接口和token注入

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 // 导出一个axios的实例  而且这个实例要有请求拦截器 响应拦截器
 import axios from 'axios'
 import { Message } from 'element-ui'
-import { store } from '@/store'
+import store from '@/store'
 // 创建一个axios的实例
 const service = axios.create({
   // 设置axios请求的基础的基础地址
@@ -11,10 +11,14 @@ const service = axios.create({
 })
 // 请求拦截器
 service.interceptors.request.use(config => {
+  // 在这个位置需要统一的去注入token
   if (store.getters.token) {
-    config.headers.Authorization = `Bearer ${store.getters.token}`
+    // 如果token存在 注入token
+    config.headers['Authorization'] = `Bearer ${store.getters.token}`
   }
-  return config
+  return config // 必须返回配置
+}, error => {
+  return Promise.reject(error)
 })
 
 // 响应拦截器
