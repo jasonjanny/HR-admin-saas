6d998646a997a4a0faec23d8b4dfd88bc864543b
janny <2431349655@qq.com>
2020-12-11 10:39:53 +0800

    Token失效的主动介入

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,8 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
 import store from '@/store'
+import { getTimeStamp } from './auth'
+import router from '@/router'
 // 创建一个axios的实例
 const service = axios.create({
   // 设置axios请求的基础的基础地址
@@ -9,12 +11,21 @@ const service = axios.create({
   // 定义5秒超时
   timeout: 5000
 })
+
+const timeout = 7200
 // 请求拦截器
 service.interceptors.request.use(config => {
   // 在这个位置需要统一的去注入token
   if (store.getters.token) {
-    // 如果token存在 注入token
-    config.headers['Authorization'] = `Bearer ${store.getters.token}`
+    // 超时
+    if (isCheckTimeout()) {
+      store.dispatch('user/logout')
+      router.push('/login')
+      return Promise.reject(new Error('token已经失效，请重新登录'))
+    } else {
+      // 如果token存在 注入token
+      config.headers['Authorization'] = `Bearer ${store.getters.token}`
+    }
   }
   return config // 必须返回配置
 }, error => {
@@ -37,5 +48,15 @@ service.interceptors.response.use(res => {
   Message.error(err.message)
   return Promise.reject(new Error(err.message))
 })
-export default service // 导出axios实例
 
+function isCheckTimeout() {
+  // 获取当前时间戳
+  const now = Date.now()
+  // 获取登录时保存的时间戳
+  const savedTime = getTimeStamp()
+  console.log(now)
+  console.log(savedTime)
+  // 将两个时间戳进行判断
+  return (now - savedTime) / 1000 >= timeout
+}
+export default service // 导出axios实例
