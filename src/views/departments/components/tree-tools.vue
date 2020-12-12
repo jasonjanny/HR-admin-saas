95f74cc394c749f84129735f3ebf33051e0cf7e4
janny <2431349655@qq.com>
2020-12-12 11:11:44 +0800

    监听下拉菜单command事件，调用删除接口

--- src/views/departments/components/tree-tools.vue
+++ src/views/departments/components/tree-tools.vue
@@ -9,15 +9,15 @@
         <el-col>{{ data.manage }}</el-col>
         <el-col>
           <!-- 下拉菜单 element -->
-          <el-dropdown>
+          <el-dropdown @command="operateDepts">
             <span>
               操作<i class="el-icon-arrow-down" />
             </span>
             <!-- 下拉菜单 -->
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>添加子部门</el-dropdown-item>
-              <el-dropdown-item v-if="!isRoot">编辑部门</el-dropdown-item>
-              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+              <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot" command="edit">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot" command="del">删除部门</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
         </el-col>
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import { delDepartments } from '@/api/departments'
 export default {
   props: {
     data: {
@@ -36,6 +37,29 @@ export default {
     isRoot: {
       type: Boolean,
       default: false
+    }
+  },
+  methods: {
+    operateDepts(type) {
+      if (type === 'add') {
+        // 添加子部门
+
+      } else if (type === 'edit') {
+        // 编辑部门
+
+      } else {
+        // 删除部门
+        this.$confirm('确定要删除该部门吗', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          // 如果点击了确定就会进入then
+          return delDepartments(this.data.id) // 返回promise对象
+        }).then(() => {
+
+        })
+      }
     }
   }
 
