1ba397bbc76ec58bc291f2478cca7ee3e456e12d
janny <2431349655@qq.com>
2020-12-12 10:34:20 +0800

    封装删除部门的请求

--- src/api/departments.js
+++ src/api/departments.js
@@ -3,6 +3,14 @@ import request from '@/utils/request'
 // 获取部门列表
 export function getDepartments() {
   return request({
-    url: 'company/department'
+    url: '/company/department'
+  })
+}
+
+// 删除部门
+export function delDepartments(id) {
+  return request({
+    method: 'delete',
+    url: '/company/department/' + id
   })
 }
