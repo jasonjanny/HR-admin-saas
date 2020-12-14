9b8d402c94ee2871603210dbc886db2c475b4623
janny <2431349655@qq.com>
2020-12-14 18:27:02 +0800

    实现公司信息的结构布局

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -4,7 +4,12 @@
       <el-card>
         <el-tabs v-model="activeName">
           <el-tab-pane name="role" label="角色管理">
-            <el-row style="height:80px" type="flex" justify="center" align="middle">
+            <el-row
+              style="height: 80px"
+              type="flex"
+              justify="center"
+              align="middle"
+            >
               <el-col>
                 <el-button
                   icon="el-icon-plus"
@@ -14,34 +19,64 @@
                 </el-button>
               </el-col>
             </el-row>
+            <!-- 表格 -->
+            <el-table border="" :data="roleList">
+              <el-table-column label="序号" width="120">
+                <template slot-scope="scope">
+                  {{ scope.$index + 1 }}
+                </template>
+              </el-table-column>
+              <el-table-column label="角色名称" width="240" prop="name" />
+              <el-table-column label="描述" prop="description" />
+              <el-table-column label="操作">
+                <el-button size="small" type="primary">编辑</el-button>
+                <el-button size="small" type="danger">删除</el-button>
+              </el-table-column>
+            </el-table>
+            <!-- 分页 -->
+            <el-row
+              type="flex"
+              justify="center"
+              align="middle"
+              style="height: 60px"
+            >
+              <el-pagination
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total"
+                :page-size="pageSetting.pagesize"
+                :page-sizes="[2, 5, 10, 20]"
+                @current-change="currentChange"
+                @size-change="sizeChange"
+              />
+            </el-row>
           </el-tab-pane>
-          <el-tab-pane name="company" label="公司信息">公司信息</el-tab-pane>
+          <el-tab-pane name="company" label="公司信息">
+            <el-alert
+              title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改"
+              type="info"
+              show-icon
+              :closable="false"
+            />
+            <el-form label-width="120px" style="margin-top:50px">
+              <el-form-item label="企业名称">
+                <el-input style="width:400px" />
+              </el-form-item>
+              <el-form-item label="公司地址">
+                <el-input style="width:400px" />
+              </el-form-item>
+              <el-form-item label="公用电话">
+                <el-input style="width:400px" />
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input style="width:400px" />
+              </el-form-item>
+              <el-form-item label="备注">
+                <el-input style="width:400px" />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
         </el-tabs>
-        <!-- 表格 -->
-        <el-table border="" :data="roleList">
-          <el-table-column label="序号" width="120">
-            <template slot-scope="scope">
-              {{ scope.$index + 1 }}
-            </template>
-          </el-table-column>
-          <el-table-column label="角色名称" width="240" prop="name" />
-          <el-table-column label="描述" prop="description" />
-          <el-table-column label="操作">
-            <el-button size="small" type="primary">编辑</el-button>
-            <el-button size="small" type="danger">删除</el-button>
-          </el-table-column>
-        </el-table>
-        <!-- 分页 -->
-        <el-row type="flex" justify="center" align="middle" style="height:60px">
-          <el-pagination
-            layout="total, sizes, prev, pager, next, jumper"
-            :total="total"
-            :page-size="pageSetting.pagesize"
-            :page-sizes="[2,5,10,20]"
-            @current-change="currentChange"
-            @size-change="sizeChange"
-          />
-        </el-row>
+
       </el-card>
     </div>
   </div>
