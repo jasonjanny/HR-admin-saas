98a369319506c6a6c9ec16cb427fcf3234f0c2fb
janny <2431349655@qq.com>
2020-12-14 16:18:18 +0800

    获取公司角色数据并在表格上进行显示

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,23 +1,69 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
-        <el-menu-item index="1">公司设置</el-menu-item>
-      </el-menu>
+      <el-card>
+        <el-tabs v-model="activeName">
+          <el-tab-pane name="role" label="角色管理">
+            <el-row style="height:80px" type="flex" justify="center" align="middle">
+              <el-col>
+                <el-button
+                  icon="el-icon-plus"
+                  size="small"
+                  type="primary"
+                >新增角色
+                </el-button>
+              </el-col>
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane name="company" label="公司信息">公司信息</el-tab-pane>
+        </el-tabs>
+        <!-- 表格 -->
+        <el-table border="" :data="roleList">
+          <el-table-column label="序号" width="120">
+            <template slot-scope="scope">
+              {{ scope.$index + 1 }}
+            </template>
+          </el-table-column>
+          <el-table-column label="角色名称" width="240" prop="name" />
+          <el-table-column label="描述" prop="description" />
+          <el-table-column label="操作">
+            <el-button size="small" type="success">分配权限</el-button>
+            <el-button size="small" type="primary">编辑</el-button>
+            <el-button size="small" type="danger">删除</el-button>
+          </el-table-column>
+        </el-table>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import { getRoleList } from '@/api/setting'
 export default {
   data() {
     return {
-      activeIndex: '1'
+      activeName: 'role',
+      roleList: [],
+      pageSetting: {
+        page: 1,
+        pagesize: 5
+      },
+      total: 0
+    }
+  },
+  created() {
+    this.getRoleList()
+  },
+  methods: {
+    async getRoleList() {
+      const { total, rows } = await getRoleList(this.pageSetting)
+      this.total = total
+      this.roleList = rows
+      console.log(this.roleList)
     }
   }
 }
 </script>
 
 <style>
-
 </style>
