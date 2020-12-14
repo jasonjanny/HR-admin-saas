9e34e000798c5d5a0eb2b3ff121919cf230908ea
janny <2431349655@qq.com>
2020-12-14 20:46:17 +0800

    封装删除角色的请求

--- src/api/setting.js
+++ src/api/setting.js
@@ -15,3 +15,11 @@ export function getCompanyInfo(companyId) {
     url: '/company/' + companyId
   })
 }
+
+// 删除角色
+export function delRole(id) {
+  return request({
+    method: 'delete',
+    url: '/sys/role/' + id
+  })
+}
