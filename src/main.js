c7fbd64443ffa810c3af075fd919cbfc31661669
janny <2431349655@qq.com>
2020-12-14 17:32:31 +0800

    element-ui 设置为默认中文显示

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@/styles/index.scss' // global css
 
@@ -16,9 +15,9 @@ import '@/icons' // icon
 import '@/permission' // permission control
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+// Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
-// Vue.use(ElementUI)
+Vue.use(ElementUI)
 
 // 注册自定义指令
 import * as directives from '@/directives'
