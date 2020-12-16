5a814e06be0ab8ae8b12064d11857a291eace20d
janny <2431349655@qq.com>
2020-12-16 17:17:24 +0800

    封装删除员工的请求

--- src/api/employees.js
+++ src/api/employees.js
@@ -14,3 +14,11 @@ export function getEmployeesList(params) {
     params
   })
 }
+
+// 删除员工
+export function delEmployee(id) {
+  return request({
+    url: '/sys/user/' + id,
+    method: 'delete'
+  })
+}
