758c83776b0f2711d6c8b26deddc601b2181b7ad
janny <2431349655@qq.com>
2020-12-17 11:24:46 +0800

    注册全局的导入excel组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,11 @@
 // 该文件负责所有的公共的组件的全局注册   Vue.use
 import PageTools from '@/components/PageTools'
+import UploadExcel from '@/components/uploadExcel'
 export default {
   install(Vue) {
     //  注册全局的通用栏组件对象
     Vue.component('PageTools', PageTools)
+    // 注册导入excel组件
+    Vue.component('UploadExcel', UploadExcel)
   }
 }
