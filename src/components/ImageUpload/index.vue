5c98adf48d54220f48d545771a82a897dc0cc156
janny <2431349655@qq.com>
2020-12-19 16:30:40 +0800

    实现图片预览

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,15 +1,41 @@
 <template>
-  <el-upload list-type="picture-card" action="#">
-    <i class="el-icon-plus" />
-  </el-upload>
+  <div>
+    <el-upload
+      list-type="picture-card"
+      action="#"
+      :file-list="fileList"
+      :on-preview="preview"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-dialog title="图片预览" :visible.sync="showDialog">
+      <img :src="imgUrl" alt="" style="width:100%">
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      showDialog: false,
+      imgUrl: '',
+      fileList: [
+        {
+          url:
+            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1608375786527&di=8318ac46843cc7e772831c1cc6a03f62&imgtype=0&src=http%3A%2F%2Fphotocdn.sohu.com%2F20160311%2Fmp62970613_1457663723054_6.jpeg'
+        }
+      ]
+    }
+  },
+  methods: {
+    preview(file) {
+      this.showDialog = true
+      this.imgUrl = file.url
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
