2b4b47d52c096c84eb710545dfacb63fb2ddd266
janny <2431349655@qq.com>
2020-12-19 16:52:26 +0800

    根据上传数量控制上传按钮、删除图片

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -5,11 +5,13 @@
       action="#"
       :file-list="fileList"
       :on-preview="preview"
+      :on-remove="handleRemove"
+      :class="{disable:fileUpload }"
     >
       <i class="el-icon-plus" />
     </el-upload>
     <el-dialog title="图片预览" :visible.sync="showDialog">
-      <img :src="imgUrl" alt="" style="width:100%">
+      <img :src="imgUrl" alt="" style="width:100%;">
     </el-dialog>
   </div>
 </template>
@@ -28,14 +30,31 @@ export default {
       ]
     }
   },
+  computed: {
+    // 设定一个计算属性 判断是否已经上传完了一张
+    fileUpload() {
+      return this.fileList.length === 1
+    }
+  },
   methods: {
     preview(file) {
       this.showDialog = true
       this.imgUrl = file.url
+    },
+    handleRemove(file, fileList) {
+      this.fileList = [...fileList]
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+ ::v-deep .disable .el-upload--picture-card {
+  display: none
+}
+::v-deep.upload{
+     img{
+        object-fit: cover;
+    }
+}
 </style>
