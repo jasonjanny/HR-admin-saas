485d2e0be2400b9f926b862a272a9158f4383e3e
janny <2431349655@qq.com>
2020-12-19 16:05:11 +0800

    初步封装上传图片组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,14 @@
 // 该文件负责所有的公共的组件的全局注册   Vue.use
 import PageTools from '@/components/PageTools'
 import UploadExcel from '@/components/uploadExcel'
+import ImageUpload from '@/components/ImageUpload'
 export default {
   install(Vue) {
     //  注册全局的通用栏组件对象
     Vue.component('PageTools', PageTools)
     // 注册导入excel组件
     Vue.component('UploadExcel', UploadExcel)
+    // 注册上传图片组件
+    Vue.component('ImageUpload', ImageUpload)
   }
 }
