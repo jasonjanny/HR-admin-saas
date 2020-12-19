aeb3f08f75a99d98e14e631f0bef156c4c2da12d
janny <2431349655@qq.com>
2020-12-19 11:23:49 +0800

    实现用户名和密码的修改、校验表单

--- src/views/employees/detail/index.vue
+++ src/views/employees/detail/index.vue
@@ -5,16 +5,16 @@
         <el-tabs>
           <el-tab-pane label="登录账户设置">
             <!-- 放置表单 -->
-            <el-form label-width="120px" style="margin-left: 200px; margin-top:30px">
-              <el-form-item label="姓名:">
+            <el-form ref="userForm" label-width="120px" style="margin-left: 200px; margin-top:30px" :model="formData" :rules="rules">
+              <el-form-item label="姓名:" prop="username">
                 <el-input v-model="formData.username" style="width:300px" />
               </el-form-item>
-              <el-form-item label="密码:">
+              <el-form-item label="密码:" prop="newPassword">
                 <el-input v-model="formData.newPassword" style="width:300px" type="password" />
               </el-form-item>
               <el-form-item>
-                <el-button type="primary">更新</el-button>
-                <el-button>取消</el-button>
+                <el-button type="primary" @click="updateAccount">更新</el-button>
+                <el-button @click="$router.back()">取消</el-button>
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -32,6 +32,7 @@
 
 <script>
 import { getUserDetailById } from '@/api/user'
+import { saveUserDetailById } from '@/api/employees'
 export default {
   data() {
     return {
@@ -39,6 +40,15 @@ export default {
       formData: {
         username: '',
         newPassword: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '姓名不能为空', trigger: 'blur' }
+        ],
+        newPassword: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 9, message: '密码长度6-9位', trigger: 'blur' }
+        ]
       }
     }
   },
@@ -49,6 +59,16 @@ export default {
     async getUserDetailById() {
       const data = await getUserDetailById(this.userId)
       this.formData = data
+      console.log(data)
+    },
+    async updateAccount() {
+      try {
+        await this.$refs.userForm.validate()
+        await saveUserDetailById({ ...this.formData, password: this.formData.newPassword })
+        this.$message.success('用户数据更新成功')
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
