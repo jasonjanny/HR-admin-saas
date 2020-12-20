a76166ea2a64188a1eff7cba873d82b9d524a41b
janny <2431349655@qq.com>
2020-12-20 11:20:14 +0800

    员工列表显示图片

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -35,6 +35,17 @@
           </template>
         </el-table-column>
         <el-table-column label="姓名" sortable="" prop="username" />
+        <el-table-column label="头像" align="center" width="100px">
+          <template slot-scope="{row}">
+            <img
+              slot="reference"
+              v-imageerror="require('@/assets/common/bigUserHeader.png')"
+              :src="row.staffPhoto "
+              style="border-radius: 50%; width: 80px; height: 80px; padding: 10px"
+              alt=""
+            >
+          </template>
+        </el-table-column>
         <el-table-column label="工号" sortable="" prop="workNumber" />
         <el-table-column
           label="聘用形式"
