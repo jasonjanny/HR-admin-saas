068680f4927e75377cf0d8a243770627b011d52a
janny <2431349655@qq.com>
2020-12-25 11:58:30 +0800

    实现月份的选择

--- src/views/dashboard/components/work-calendar.vue
+++ src/views/dashboard/components/work-calendar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-row type="flex" justify="end">
-      <el-select size="small" style="width: 120px" placeholder="请选择年份">
+      <el-select v-model="currentYear" size="small" style="width: 120px" placeholder="请选择年份" @change="changeDate">
         <el-option
           v-for="item in yearList"
           :key="item"
@@ -10,9 +10,11 @@
         >{{ item }}年</el-option>
       </el-select>
       <el-select
+        v-model="currentMonth"
         size="small"
         style="width: 120px; margin-left: 10px"
         placeholder="请选择月份"
+        @change="changeDate"
       >
         <el-option
           v-for="item in 12"
@@ -37,14 +39,28 @@ export default {
   data() {
     return {
       currentDate: null,
+      currentMonth: null,
+      currentYear: null,
       yearList: []
     }
   },
   created() {
+    // 获取当前年份和月份
+    this.currentYear = this.startDate.getFullYear()
+    this.currentMonth = this.startDate.getMonth() + 1
     this.yearList = Array.from(Array(11), (val, index) => {
       const now = new Date()
       return index + now.getFullYear() - 5
     })
+  },
+  methods: {
+    changeDate() {
+      const year = this.currentYear
+      const month = this.currentMonth
+      const dateStr = `${year}-${month}-1`
+      //   console.log(dateStr)
+      this.currentDate = new Date(dateStr)
+    }
   }
 }
 </script>
