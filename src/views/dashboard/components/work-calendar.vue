8df60dd5883e88bf144329d8825dbcf47725a91a
janny <2431349655@qq.com>
2020-12-25 14:30:30 +0800

    完成工作日历组件

--- src/views/dashboard/components/work-calendar.vue
+++ src/views/dashboard/components/work-calendar.vue
@@ -24,12 +24,25 @@
         >{{ item }}月</el-option>
       </el-select>
     </el-row>
-    <el-calendar v-model="currentDate" />
+    <el-calendar v-model="currentDate">
+      <template slot="dateCell" slot-scope="scope" class="content">
+        <div class="date-content">
+          <span class="text">{{ scope.data.day | getDay }}</span>
+          <span v-if="isWeek(scope.date)" class="rest">休</span>
+        </div>
+      </template>
+    </el-calendar>
   </div>
 </template>
 
 <script>
 export default {
+  filters: {
+    getDay(value) {
+      const date = value.split('-')[2]
+      return date.startsWith('0') ? date.substr(1) : date
+    }
+  },
   props: {
     startDate: {
       type: Date,
@@ -60,10 +73,51 @@ export default {
       const dateStr = `${year}-${month}-1`
       //   console.log(dateStr)
       this.currentDate = new Date(dateStr)
+    },
+    isWeek(value) {
+      return value.getDay() === 6 || value.getDay() === 0
     }
   }
 }
 </script>
 
-<style>
+<style  lang="scss" scoped>
+ ::v-deep .el-calendar-day {
+  height:  auto;
+ }
+ ::v-deep .el-calendar-table__row td,::v-deep .el-calendar-table tr td:first-child,  ::v-deep .el-calendar-table__row td.prev{
+  border:none;
+ }
+.date-content {
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  font-size: 14px;
+}
+.date-content .rest {
+  color: #fff;
+  border-radius: 50%;
+  background: rgb(250, 124, 77);
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  display: inline-block;
+  font-size: 12px;
+  margin-left: 10px;
+}
+.date-content .text{
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+ display: inline-block;
+
+}
+ ::v-deep .el-calendar-table td.is-selected .text{
+   background: #409eff;
+   color: #fff;
+   border-radius: 50%;
+ }
+ ::v-deep .el-calendar__header {
+   display: none
+ }
 </style>
